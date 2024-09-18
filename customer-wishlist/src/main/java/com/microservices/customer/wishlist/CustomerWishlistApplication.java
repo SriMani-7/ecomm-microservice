ea2c94503d087007266acad87309640b40043c4e
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-09-18 10:15:45 +0530

    wishlist-created

---
+++ customer-wishlist/src/main/java/com/microservices/customer/wishlist/CustomerWishlistApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.customer.wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerWishlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerWishlistApplication.class, args);
+	}
+
+}
---
+++ customer-wishlist/src/test/java/com/microservices/customer/wishlist/CustomerWishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.customer.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerWishlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
