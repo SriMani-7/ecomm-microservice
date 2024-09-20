bdf341d2e42add632854571eb70cbf39364370cd
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-09-20 16:32:57 +0530

    api-gateway

---
+++ api-gateway/src/main/java/com/microservices/gateway/GatewayConfig.java
@@ -0,0 +1,35 @@
+package com.microservices.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+	private final JwtAuthenticationFilter filter;
+
+	public GatewayConfig(JwtAuthenticationFilter filter) {
+		this.filter = filter;
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder.routes()
+//				.route("user-service",
+//						r -> r.path("/v1/user/**").filters(f -> f.filter(filter)).uri("lb://user-service"))
+//
+//				.route("job-service",
+//						r -> r.path("/v1/job-service/**").filters(f -> f.filter(filter)).uri("lb://job-service"))
+//
+//				.route("notification-service",
+//						r -> r.path("/v1/notification/**").filters(f -> f.filter(filter))
+//								.uri("lb://notification-service"))
+//
+//				.route("auth-service", r -> r.path("/v1/auth/**").uri("lb://auth-service"))
+//
+//				.route("file-storage",
+//						r -> r.path("/v1/file-storage/**").filters(f -> f.filter(filter)).uri("lb://file-storage"))
+				.build();
+	}
+}
