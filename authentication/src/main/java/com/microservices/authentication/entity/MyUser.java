afaf5a35a3c80e6b5d8b8a7cf0e4a406f7366181
vishnu <vishnuvardhankumpati@gmail.com>
2024-09-21 14:41:39 +0530

    entities

---
+++ authentication/src/main/java/com/microservices/authentication/entity/MyUser.java
@@ -0,0 +1,72 @@
+package com.microservices.authentication.entity;
+
+import java.time.LocalDateTime;
+
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "users")
+public class MyUser {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+	
+	private String username;
+	
+    @Min(19)
+    @Max(100)
+	private Integer age;
+    
+	@Column(nullable = false)
+	private String  email;
+    
+	@Max(9999999999L)
+	private Long contactNo;
+
+	private String password;
+	
+    @CreationTimestamp
+	private LocalDateTime createdAt;
+    
+	@UpdateTimestamp
+	private LocalDateTime lastSeen;
+
+	private String userType;
+	
+	
+	@Enumerated(EnumType.STRING)
+	private UserStatus status;
+	
+	public enum UserStatus{
+		UNDER_REVIEW,
+		SUSPENDED,
+		ACTIVE
+	}
+	
+	
+	
+	
+	
+
+}
