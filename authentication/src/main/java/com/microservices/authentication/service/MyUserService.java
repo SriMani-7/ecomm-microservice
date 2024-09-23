81106ce4e8a6ad551f3b04678ab5e697de8ae4b1
SHIVA PRASAD <SHIVA PRASAD@Prasad>
2024-09-23 15:32:24 +0530

    fetching users moved from admin to authentication

---
+++ authentication/src/main/java/com/microservices/authentication/service/MyUserService.java
@@ -0,0 +1,28 @@
+package com.microservices.authentication.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.microservices.authentication.Repo.UserRepo;
+import com.microservices.authentication.entity.MyUser;
+import com.microservices.authentication.entity.MyUser.UserStatus;
+
+@Service
+public class MyUserService {
+	@Autowired
+	private UserRepo dao;
+
+	public List<MyUser> getAllUsers() {
+		// TODO Auto-generated method stub
+		return dao.getAllUsers();
+	}
+
+	public void updateUserStatus(long userId, UserStatus status) {
+		// TODO Auto-generated method stub
+		dao.updateUserStatus(userId, status);
+
+	}
+
+}
