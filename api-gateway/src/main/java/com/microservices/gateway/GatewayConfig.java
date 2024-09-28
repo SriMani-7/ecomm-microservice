97fa5528b3cfa394c563f0d1168686379de6e336
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-09-28 10:15:05 +0530

    JWT related stuff removed from api-gateway

--- api-gateway/src/main/java/com/microservices/gateway/GatewayConfig.java
+++ api-gateway/src/main/java/com/microservices/gateway/GatewayConfig.java
@@ -7,15 +7,10 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfig {
-	private final JwtAuthenticationFilter filter;
-
-	public GatewayConfig(JwtAuthenticationFilter filter) {
-		this.filter = filter;
-	}
 
 	@Bean
-	public RouteLocator routes(RouteLocatorBuilder builder) {
-		return builder.routes().route("admin-service", t -> t.path("/admin/*").uri("lb://admin"))
+	RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder.routes().route(t -> t.path("/admin/**").uri("lb://authentication"))
 				.route(r -> r.path("/auth/**").uri("lb://authentication")).build();
 	}
 }
