4a38d25e31e4040ca64efe118e8856d7b9d52056
vishnu <vishnuvardhankumpati@gmail.com>
2024-10-04 10:23:01 +0530

    contoller for forgotpassword

--- app/src/main/java/com/microservices/app/controller/LoginRegisterController.java
+++ app/src/main/java/com/microservices/app/controller/LoginRegisterController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -115,6 +116,7 @@ public class LoginRegisterController {
 	public ResponseEntity<String> verifyEmailOTP(@RequestBody OTPVerifyRequest otpVerifyRequest) {
 		return service.verifyEmail(otpVerifyRequest);
 	}
+	@RequestMapping("/")
 	@PostMapping("/forgotpassword")
 	public String passwordRecovery(@RequestParam String email,Model model) {
 		String message = service.existsByEmail(email);
