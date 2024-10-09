155504cec751b0faa4e1c1b38dc907b8690ef593
Sri Manikanta <84219628+SriMani-7@users.noreply.github.com>
2024-10-09 21:39:21 +0530

    admin with restricted OAuth finally

--- admin-client/src/main/java/com/microservices/admin/OAuthConfig.java
+++ admin-client/src/main/java/com/microservices/admin/OAuthConfig.java
@@ -1,5 +1,6 @@
 package com.microservices.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,19 +12,26 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class OAuthConfig {
 
+	@Autowired
+	private OAuthLoginHandler oAuthLoginHandler;
+
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		// @formatter:off
 		return http
 				.csrf(CsrfConfigurer::disable)
 				.authorizeHttpRequests(requests -> requests
-					.requestMatchers("/admin").authenticated()
+					.requestMatchers("/admin/**").authenticated()
 					.anyRequest().permitAll()
 				)
 				.oauth2Login(oAuth -> oAuth
 					.defaultSuccessUrl("/admin")
 					.loginPage("/")
+					.successHandler(oAuthLoginHandler)
 				)
+				.exceptionHandling(config -> {
+					config.accessDeniedPage("/access-denied");
+				})
 				.build();
 		// @formatter:on
 	}
