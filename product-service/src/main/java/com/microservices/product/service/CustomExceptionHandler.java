59234b2f2555695168cd41767476f404bdfdd657
venkatasagarearla <earlavenkatasagar@gmail.com>
2024-10-10 08:02:26 +0530

    some changes in orders

--- product-service/src/main/java/com/microservices/product/service/CustomExceptionHandler.java
+++ product-service/src/main/java/com/microservices/product/service/CustomExceptionHandler.java
@@ -5,14 +5,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.microservices.product.service.exception.OutOfStockException;
+import com.microservices.product.service.exception.ProductNotFoundException;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
-    @ExceptionHandler(NoSuchElementException.class)
+    @ExceptionHandler(value = {NoSuchElementException.class ,ProductNotFoundException.class})
     public ResponseEntity<String> handleNoSuchElement(NoSuchElementException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(OutOfStockException.class)
+    public ResponseEntity<String> outofStockExceptionHandler(NoSuchElementException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
 }
--- product-service/src/main/java/com/microservices/product/service/dto/CheckoutRequest.java
+++ product-service/src/main/java/com/microservices/product/service/dto/CheckoutRequest.java
@@ -1,8 +1,10 @@
 package com.microservices.product.service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class CheckoutRequest {
 	private String address;
 	private String paymentType;
